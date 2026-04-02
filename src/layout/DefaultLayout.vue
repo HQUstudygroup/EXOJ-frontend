e9540ac8b105363779a53399a5e326ad25d9379a
swuY <swu341457@gmail.com>
2026-04-03 03:06:45 +0800

    feat(./layout): 完成页面布局搭建

---
+++ src/layout/DefaultLayout.vue
@@ -0,0 +1,26 @@
+<template>
+    <n-layout class="h-screen" content-class="flex flex-col">
+        <n-layout-header bordered class="h-1/15 shrink-0 p-6"> Header </n-layout-header>
+        <n-layout has-sider class="flex-1">
+            <n-layout-sider
+                bordered
+                content-style="padding: 24px;"
+                :native-scrollbar="false"
+                collapse-mode="width"
+                show-trigger="arrow-circle"
+                collapsed-width="0"
+                collapsed-trigger-style="top: 250px; right: -25px"
+                trigger-style="top: 250px"
+            >
+                SideBar
+            </n-layout-sider>
+            <n-layout-content bordered content-class="p-18" :native-scrollbar="false">
+                <router-view />
+            </n-layout-content>
+        </n-layout>
+    </n-layout>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/view/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <n-h2>Content</n-h2>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
