a45a7ff0ed17e7da2b18f3c994ff183e362fc1ac
张铭鸿 <ssxt888@gmail.com>
2026-04-19 08:48:22 +0800

    feat(./components): 更新Ai Assistant 组件

---
+++ src/components/AssistantChat/MessageBubble.vue
@@ -0,0 +1,58 @@
+<template>
+    <t-chat-message
+        v-for="msg in AiAssistantStore.messages"
+        animation="circle"
+        :avatar="
+            msg.role === 'user'
+                ? 'https://tdesign.gtimg.com/site/avatar.jpg'
+                : 'https://tdesign.gtimg.com/site/chat-avatar.png'
+        "
+        :chat-content-props="{
+            markdown: { options: options },
+        }"
+        :name="msg.role === 'user' ? 'User' : 'Asssistant'"
+        :role="msg.role"
+        :placement="msg.role === 'user' ? 'right' : 'left'"
+        :status="msg.isPending ? 'pending' : ''"
+        :variant="msg.role === 'user' ? 'base' : 'outline'"
+        :content="[
+            {
+                type: msg.role === 'user' ? 'text' : 'markdown',
+                data: msg.content,
+            },
+        ]"
+    >
+    </t-chat-message>
+</template>
+
+<script setup lang="ts">
+import { useAiAssistantStore } from '@/store/AiAssistant';
+
+const AiAssistantStore = useAiAssistantStore();
+
+import katex from 'katex';
+
+/**
+ * 配置全局数学公式渲染引擎 (KaTeX)
+ *
+ * 将 katex 实例挂载至 window 全局对象，以供AI流式渲染使用。
+ * @type {import('katex')}
+ */
+
+(window as any).katex = katex;
+
+const options = {
+    engine: {
+        syntax: {
+            mathBlock: {
+                engine: 'katex',
+            },
+            inlineMath: {
+                engine: 'katex',
+            },
+        },
+    },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/AssistantChat/MessageInputBox.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="h-25 pb-5">
+        <t-chat-sender
+            v-model="prompt"
+            :loading="AiAssistantStore.isInputLoading"
+            :textarea-props="{
+                placeholder: '请输入消息...',
+                class: 'max-h-12 overflow-hidden',
+            }"
+            @send="handlerSendMessage"
+            @stop="AiAssistantStore.stopCurrentTask"
+        >
+            <template #footer-prefix>
+                <n-select
+                    v-model:value="value"
+                    :options="assistantModelOptions"
+                    :consistent-menu-width="false"
+                    class="w-25 [&_>*]:rounded-full"
+                />
+            </template>
+        </t-chat-sender>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useAiAssistantStore } from '@/store/AiAssistant';
+
+const value = ref('openclaw/default');
+const prompt = ref('');
+
+const assistantModelOptions = [
+    {
+        label: '默认模型',
+        value: 'openclaw/default',
+    },
+    {
+        label: '千问',
+        value: 'openclaw/qwen',
+    },
+    {
+        label: 'deepseek',
+        value: 'openclaw/deepseek',
+    },
+    {
+        label: 'glm-4.7',
+        value: 'openclaw/glm-4.7',
+    },
+];
+
+const AiAssistantStore = useAiAssistantStore();
+
+/**
+ * 发送框发送消息的回调函数。
+ * 进入AiAssistantStore 发起 AI 对话请求
+ */
+const handlerSendMessage = function () {
+    if (AiAssistantStore.isInputLoading || !prompt.value) return;
+
+    AiAssistantStore.requestAssistant(prompt.value);
+    prompt.value = '';
+};
+</script>
+
+<style scoped></style>
