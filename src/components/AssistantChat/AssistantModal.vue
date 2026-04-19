02de203dbbaf14dcb6bf99c1c60d3e1382a71bf7
swuY <swu341457@gmail.com>
2026-04-20 00:21:32 +0800

    style(./component): 完成 AI 助手模态框响应式

--- src/components/AssistantChat/AssistantModal.vue
+++ src/components/AssistantChat/AssistantModal.vue
@@ -8,10 +8,10 @@
             :segmented="{ content: true, footer: true }"
             title="AI 助手"
             preset="card"
-            class="w-500px h-800px rounded-8 position-fixed right-3 bottom-3 shadow-[0_4px_16px_rgba(0,0,0,0.5)]"
+            class="w-1/4vw h-5/6vh rounded-8 position-fixed right-5 bottom-10 shadow-[0_4px_16px_rgba(0,0,0,0.5)]"
         >
             <template #header-extra>
-                <n-button class="mr-10 rounded-5" @click="handleNewSession"> new session </n-button>
+                <n-button class="mr-3 rounded-5" @click="handleNewSession"> New Session </n-button>
             </template>
 
             <MessageArea></MessageArea>
